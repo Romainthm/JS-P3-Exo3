bdb684b09d99e96b37cdf7b86fa72ce4cddf6d93
romain <romain.thomine007@gmail.com>
2019-04-11 13:34:31 +0200

    fini

---
+++ script.js
@@ -0,0 +1,4 @@
+var button=document.getElementsByClassName('color')
+button[0].addEventListener('click',function(){document.getElementById('text').style.color='green'});
+button[1].addEventListener('click',function(){document.getElementById('text').style.color='red'});
+button[2].addEventListener('click',function(){document.getElementById('text').style.color='blue'});
